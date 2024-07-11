83228e8964286eb8c6eb308697330badc44db36f
Kristawan Jaroenrad <kristawanja@gmail.com>
2024-07-12 00:21:15 +0700

    create a notification sound and complete the front end to back end message system

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const express = require("express");
-const path = require("node:path")
+const path = require("node:path");
+const { sleep } = require("openai/core.js");
+var bodyParser = require('body-parser')
 
 const app = express();
 
 app.set("view engine", "ejs")
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(bodyParser.json())
+
 app.get('/', (req, res)=>{
     res.render("index")
+})
+
+// Get the response from openAI
+app.post('/koachan', async (req, res) => {
+    console.log(req.body)
+    await sleep(1000) // test code
+    res.json({role: "assistance", message: "hello"})
 })
 
 // Listening on port 2000.
